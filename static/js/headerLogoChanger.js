318033723fbb2575db5a98148d0c006740e594a4
x5dfg / dcore <142392565+x5dfg@users.noreply.github.com>
2024-08-29 21:12:33 +0300

    Update headerLogoChanger.js

--- static/js/headerLogoChanger.js
+++ static/js/headerLogoChanger.js
@@ -2,6 +2,7 @@ document.addEventListener('DOMContentLoaded', function() {
     setTimeout(function() {
         // Получаем все элементы с классом 'logo'
         const logos = document.querySelectorAll('.logo');
+        const favicon = document.querySelector('link[rel="shortcut icon"]');
 
         // Функция для загрузки изображения и установки его в качестве источника
         function loadImage(url) {
@@ -9,16 +10,30 @@ document.addEventListener('DOMContentLoaded', function() {
                 .then(response => response.blob())
                 .then(blob => URL.createObjectURL(blob));
         }
-        // Ссылка на изображение
-        const imageUrl = 'https://cdn.discordapp.com/avatars/1261251971315404812/285485f3cab67f5cf819bd35198e50ff.png?size=512'
-        // Загрузка изображения и установка его в качестве источника
-        loadImage(imageUrl).then(blobUrl => {
-            logos.forEach(function(logo) {
-                logo.src = blobUrl; // Устанавливаем новый src
-                logo.style.opacity = 1;
+
+        // Функция для получения avatar_url из GitHub API
+        function fetchAvatarUrl() {
+            return fetch('https://api.github.com/users/x5dfg')
+                .then(response => response.json())
+                .then(data => data.avatar_url);
+        }
+
+        // Получаем URL аватара и загружаем изображение
+        fetchAvatarUrl().then(imageUrl => {
+            loadImage(imageUrl).then(blobUrl => {
+                logos.forEach(function(logo) {
+                    logo.src = blobUrl; // Устанавливаем новый src для логотипов
+                    logo.style.opacity = 1;
+                });
+
+                if (favicon) {
+                    favicon.href = blobUrl; // Устанавливаем новый src для favicon
+                }
+            }).catch(error => {
+                console.error('Ошибка загрузки изображения:', error);
             });
         }).catch(error => {
-            console.error('Ошибка загрузки изображения:', error);
+            console.error('Ошибка получения URL аватара:', error);
         });
     }, 1000);
 });
